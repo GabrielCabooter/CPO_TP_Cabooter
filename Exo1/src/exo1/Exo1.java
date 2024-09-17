139ec08d21072d96b65673afcfc24b9b4eb3b3dc
Gabriel Cabooter <gabriel.cabooter@epfedu.fr>
2024-09-17 16:49:19 +0200

    EX 1

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+/*
+ * Gabriel
+ * Cabooter
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabri
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Bonjour");
+         System.out.println("Salut");
+         Scanner sc;
+         sc = new Scanner(System.in);
+         System.out.println("Bonjour, comment allez vous ?");
+         sc.nextLine();
+    }
+    
+}
